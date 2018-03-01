4d4b2d8682442ef336a80e196a4710569ab6d97c
zxw <123@qq.com>
2018-03-01 22:03:14 +0800

    电影订票系统

---
+++ resources/views/admin/public/side.blade.php
@@ -0,0 +1,31 @@
+<div class="left-nav">
+    <div id="side-nav">
+        <ul id="nav">
+            <li>
+                <a href="javascript:;">
+                    <i class="iconfont">&#xe6b8;</i>
+                    <cite>用户管理</cite>
+                    <i class="iconfont nav_right">&#xe697;</i>
+                </a>
+                <ul class="sub-menu">
+                    <li>
+                        <a _href="{{ url('admin/user') }}">
+                            <i class="iconfont">&#xe6a7;</i>
+                            <cite>用户列表</cite>
+
+                        </a>
+                    </li >
+                    <li>
+                        <a _href="{{ url('admin/user/create') }}">
+                            <i class="iconfont">&#xe6a7;</i>
+                            <cite>添加用户</cite>
+
+                        </a>
+                    </li>
+
+
+                </ul>
+        </ul>
+    </div>
+</div>
+<!-- <div class="x-slide_left"></div> -->
